354482b1257f95df6e2a6dfa3aec9763361ac2cc
Xavier Perez <xperez007@gmail.com>
2020-02-22 14:53:51 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,5 +1,17 @@
 require 'pry'
 
 class Doctor
+  attr_accessor :name
   
+  @@all = []
+  
+  def initialize(name, patient, appointment)
+    @name = name
+    @patient = patient
+    @appointment = appointment
+    @@all << self
+    
+  end
+end
+
   
